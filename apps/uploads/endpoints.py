e663db4fe055454fbc7164897b5e825b5bcfa07d
Azat <8280770+azataiot@users.noreply.github.com>
2025-05-17 11:54:25 +0300

    Refactor upload endpoints to consolidate routes and add listing functionality for user uploads

--- apps/uploads/endpoints.py
+++ apps/uploads/endpoints.py
@@ -3,7 +3,13 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
 
 from apps.auth.deps import active_token
-from apps.uploads.services import get_upload_by_id, delete_upload_file
+from apps.uploads.schemas import UploadRead
+from apps.uploads.services import (
+    get_upload_by_id,
+    delete_upload_file,
+    list_all_uploads,
+    list_user_uploads,
+)
 from apps.uploads.storages import get_storage
 from core.database import get_session
 
@@ -14,7 +20,7 @@ router = APIRouter()
 # POST /upload
 # Authenticated
 # ------------------------------------------
-@router.post("/upload")
+@router.post("/")
 async def upload(file: UploadFile = File(...), _=Depends(active_token)):
     storage = get_storage()
     file_name = file.filename
@@ -27,7 +33,22 @@ async def upload(file: UploadFile = File(...), _=Depends(active_token)):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.delete("/delete/{upload_id}", status_code=204)
+@router.get("/", response_model=list[UploadRead])
+async def list_all(
+    user=Depends(active_token),
+    session=Depends(get_session),
+):
+    result = []
+    """List all uploads for the authenticated user."""
+    if user.is_staff:
+        result = await list_all_uploads(session=session)
+    # If the user is not staff, return only their uploads
+    else:
+        result = await list_user_uploads(user.id, session=session)
+    return result
+
+
+@router.delete("/{upload_id}", status_code=204)
 async def delete_upload(
     upload_id: str,
     user=Depends(active_token),
