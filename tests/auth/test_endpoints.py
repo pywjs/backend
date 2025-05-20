5f0f677aab9bc5fda0bf8c53976645385e702fdb
Azat <8280770+azataiot@users.noreply.github.com>
2025-05-20 17:53:15 +0300

    Refactor authentication tests to use class-based structure; add tests for login and token endpoints

--- tests/auth/test_endpoints.py
+++ tests/auth/test_endpoints.py
@@ -1,54 +1,129 @@
 # tests/auth/test_endpoints.py
+
 import pytest
 from httpx import AsyncClient
-from tests.conftest import save_user_to_db
 
 
 # ---------------------------------------------
 # Test auth/token endpoint
 # ---------------------------------------------
-@pytest.mark.anyio
-async def test_login_token(client: AsyncClient, create_test_user):
+class TestToken:
     # Test login with valid credentials
-    data = {
-        "username": create_test_user.email,
-        "password": "test123",
-    }
-    response = await client.post("/auth/token", data=data)
-    assert response.status_code == 200
-    assert "access_token" in response.json()
-    assert "refresh_token" in response.json()
-    assert response.json()["token_type"] == "bearer"
+    @pytest.mark.anyio
+    async def test_token_success(self, client: AsyncClient, create_test_user):
+        # Test login with valid credentials
+        data = {
+            "username": create_test_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/token", data=data)
+        assert response.status_code == 200
+        assert "access_token" in response.json()
+        assert "refresh_token" in response.json()
+        assert response.json()["token_type"] == "bearer"
 
     # Test login with the wrong password
-    data["password"] = "wrong password"
-    response = await client.post("/auth/token", data=data)
-    assert response.status_code == 401
-    assert response.json()["detail"] == "Invalid credentials"
+    @pytest.mark.anyio
+    async def test_token_wrong_password(self, client: AsyncClient, create_test_user):
+        data = {
+            "username": create_test_user.email,
+            "password": "wrong password",
+        }
+        response = await client.post("/auth/token", data=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
 
     # Test login with non-existing user
-    data["username"] = "no-such-user@example.com"
-    response = await client.post("/auth/token", data=data)
-    assert response.status_code == 401
-    assert response.json()["detail"] == "Invalid credentials"
+    @pytest.mark.anyio
+    async def test_token_non_existing_user(self, client: AsyncClient):
+        data = {
+            "username": "no@example.com",
+            "password": "test123",
+        }
+        response = await client.post("/auth/token", data=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
 
     # Test login with inactive user
-    create_test_user.is_active = False
-    await save_user_to_db(create_test_user)
-
-    data["username"] = create_test_user.email
-    data["password"] = "test123"
-    response = await client.post("/auth/token", data=data)
-    assert response.status_code == 401
-    assert response.json()["detail"] == "Account is not active"
-    # reset user to active
-    create_test_user.is_active = True
+    @pytest.mark.anyio
+    async def test_token_inactive_user(self, client: AsyncClient, create_inactive_user):
+        data = {
+            "username": create_inactive_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/token", data=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Account is not active"
 
     # Test login with deleted user
-    create_test_user.is_deleted = True
-    await save_user_to_db(create_test_user)
-    data["username"] = create_test_user.email
-    data["password"] = "test123"
-    response = await client.post("/auth/token", data=data)
-    assert response.status_code == 401
-    assert response.json()["detail"] == "Invalid credentials"
+    @pytest.mark.anyio
+    async def test_token_deleted_user(self, client: AsyncClient, create_deleted_user):
+        data = {
+            "username": create_deleted_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/token", data=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
+
+
+# ---------------------------------------------
+# Test auth/login endpoint
+# ---------------------------------------------
+class TestLogin:
+    # Test login with valid credentials
+    @pytest.mark.anyio
+    async def test_login_success(self, client: AsyncClient, create_test_user):
+        data = {
+            "email": create_test_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/login", json=data)
+        assert response.status_code == 200
+        assert "access_token" in response.json()
+        assert "refresh_token" in response.json()
+        assert response.json()["token_type"] == "bearer"
+
+    # Test login with the wrong password
+    @pytest.mark.anyio
+    async def test_login_wrong_password(self, client: AsyncClient, create_test_user):
+        data = {
+            "email": create_test_user.email,
+            "password": "wrong password",
+        }
+        response = await client.post("/auth/login", json=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
+
+    # Test login with non-existing user
+    @pytest.mark.anyio
+    async def test_login_non_existing_user(self, client: AsyncClient):
+        data = {
+            "email": "no@example.com",
+            "password": "test123",
+        }
+        response = await client.post("/auth/login", json=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
+
+    # Test login with inactive user
+    @pytest.mark.anyio
+    async def test_login_inactive_user(self, client: AsyncClient, create_inactive_user):
+        data = {
+            "email": create_inactive_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/login", json=data)
+        assert response.status_code == 403
+        assert response.json()["detail"] == "Account is not active"
+
+    # Test login with deleted user
+    @pytest.mark.anyio
+    async def test_login_deleted_user(self, client: AsyncClient, create_deleted_user):
+        data = {
+            "email": create_deleted_user.email,
+            "password": "test123",
+        }
+        response = await client.post("/auth/login", json=data)
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Invalid credentials"
