d690a6cc1ba51e1a4c7fec54e0e14d2ba0b51687
Azat <8280770+azataiot@users.noreply.github.com>
2025-05-20 09:55:28 +0300

    Refactor Upload model to inherit from BaseTable and remove redundant datetime fields

--- apps/uploads/models.py
+++ apps/uploads/models.py
@@ -1,18 +1,15 @@
 # apps/uploads/models.py
 
-from sqlmodel import SQLModel, Field
-from datetime import datetime
-from ulid import ULID
-
-from utils.time import current_time
+from sqlmodel import Field
+from core.models import BaseTable
 
 
-class Upload(SQLModel, table=True):
-    id: str | None = Field(default_factory=lambda: str(ULID()), primary_key=True)
+class Upload(BaseTable, table=True):
     file_name: str
     url: str
     reference_count: int = 0
     public: bool = False
+
     # metadata fields
     title: str | None = None
     description: str | None = None
@@ -20,8 +17,6 @@ class Upload(SQLModel, table=True):
     size: int | None = None
     storage_backend: str = "local"  # local or s3
     md5: str | None = None
+
     # foreign key to user
     owner_id: str = Field(foreign_key="user.id", index=True)
-    # Datetime fields
-    created_at: datetime = Field(default_factory=current_time)
-    updated_at: datetime = Field(default_factory=current_time)
